12ff65fa9eac07bb8d05fc42deef5fa541b555dc
nzaman7878 <nzaman7878>
2025-07-03 15:09:44 +0530

    navbar updated

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
           </button>
         </div>
 
-        {/* Mobile Menu */}
+        {/* -----------Mobile Menu------ */}
         <ul
           ref={sideMenuRef}
           className='flex md:hidden flex-col gap-4 py-20 px-10 -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 fixed'
