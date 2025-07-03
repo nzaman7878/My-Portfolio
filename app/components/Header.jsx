3ca3fda12358ceef0b668839d3388a9f06cb0645
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-07-04 00:11:07 +0530

    added the header component

---
+++ app/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
+
+const Header = () => {
+  return (
+    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
+      <div>
+        <Image
+          src={assets.profile_img}
+          alt=""
+          className="rounded-full w-32"
+        />
+      </div>
+      
+      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
+        Hi! I'm Nuruzzaman
+        <Image src={assets.hand_icon} alt="" className="w-6" />
+      </h3>
+      
+      <h1 className="text-3xl sm:text-5xl lg:text-[66px] font-Ovo leading-tight">
+        Frontend Developer based in India.
+      </h1>
+      
+      <p className="text-base md:text-lg text-gray-600">
+        I am a frontend Developer from Assam, with 2 years of experience in
+        multiple companies like Microsoft, Tesla, and Apple.
+      </p>
+      
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-5">
+        <a
+          href="#contact"
+          className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2"
+        >
+          Contact Me
+          <Image src={assets.right_arrow_white} alt="" className="w-4" />
+        </a>
+        
+        <a
+          href="/sample-resume.pdf"
+          download
+          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
+        >
+          My Resume
+          <Image src={assets.download_icon} alt="" className="w-4" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
