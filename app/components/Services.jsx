d358cdcf934793583d75d27dd8f8e14c9b948600
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-07-04 21:48:45 +0530

    solve services css problem

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -13,7 +13,7 @@ const Services = () => {
         Microsoft, Tesla, and Apple.
       </p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 my-10">
         {serviceData.map(({ icon, title, description, link }, index) => (
           <div
             key={index}
